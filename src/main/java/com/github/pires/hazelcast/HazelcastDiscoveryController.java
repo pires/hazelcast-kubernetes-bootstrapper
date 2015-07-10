594643896678e468cfe5c995cea476c74e4e8d4d
Marcin Wielgus <mwielgus@google.com>
2015-07-10 15:41:54 +0200

    Get pod namespace from env variable

--- src/main/java/com/github/pires/hazelcast/HazelcastDiscoveryController.java
+++ src/main/java/com/github/pires/hazelcast/HazelcastDiscoveryController.java
@@ -97,7 +97,8 @@ public class HazelcastDiscoveryController implements CommandLineRunner {
   @Override
   public void run(String... args) {
     final String serviceName = getEnvOrDefault("HAZELCAST_SERVICE", "hazelcast");
-    final String path = "/api/v1/namespaces/default/endpoints/";
+    final String namespace = getEnvOrDefault("POD_NAMESPACE", "default");
+    final String path = String.format("/api/v1/namespaces/%s/endpoints/", namespace);
     final String domain = getEnvOrDefault("DNS_DOMAIN", "cluster.local");
     final String host = getEnvOrDefault("KUBERNETES_MASTER", "https://kubernetes.default.svc.".concat(domain));
     log.info("Asking k8s registry at {}..", host);
